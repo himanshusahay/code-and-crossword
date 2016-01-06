ada369ced231bd66c077c73f2cd3ce3a6a6604b2
neoterraforma <himanshu23@icloud.com>
2016-01-06 20:28:30 +0400

    added models and generated admin site (part 2)

--- mysite/code_and_crossword/admin.py
+++ mysite/code_and_crossword/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Code, Crossword
+
+admin.site.register(Code)
+admin.site.register(Crossword)
--- mysite/code_and_crossword/models.py
+++ mysite/code_and_crossword/models.py
@@ -3,3 +3,32 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Leaderboard(models.Model):
+	name = models.CharField(max_length = 20)
+	sub_date = models.DateTimeField('submission date')
+	score = models.IntegerField()
+	code_score = models.IntegerField()
+	crossword_score = models.IntegerField()
+
+class Code(models.Model):
+	question = models.CharField(max_length = 1000)
+	level = models.IntegerField()
+
+	def __str__(self):
+		return self.question + ". level = " + str(self.level)
+
+class Code_Answer(models.Model):
+	question = models.ForeignKey(Code, on_delete=models.CASCADE)
+	answer = models.CharField(max_length = 1000)
+
+class Crossword(models.Model):
+	question = models.CharField(max_length = 3000)
+	level = models.IntegerField()
+
+	def __str__(self):
+		return self.question + ". level = " + str(self.level)
+
+class Crossword_Answer(models.Model):
+	question = models.ForeignKey(Code, on_delete=models.CASCADE)
+	answer = models.CharField(max_length = 3000)

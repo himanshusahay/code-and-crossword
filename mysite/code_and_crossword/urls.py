fd52dfa868869d0374f28d4344678fe137e098b8
neoterraforma <himanshu23@icloud.com>
2016-01-07 01:22:53 +0400

    added views and made rudimentary templates

--- mysite/code_and_crossword/urls.py
+++ mysite/code_and_crossword/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'code_and_crossword'
 urlpatterns = [
-		url(r'^$', views.index, name='index'),
-	]
+	url(r'^$', views.index, name='index'),
+    url(r'^(?P<level>[0-9]+)/code/$', views.code, name='code'),
+    url(r'^(?P<level>[0-9]+)/crossword/$', views.crossword, name='crossword'),    
+    url(r'^leaderboard', views.leaderboard, name='leaderboard'),
+]
 
--- mysite/code_and_crossword/views.py
+++ mysite/code_and_crossword/views.py
@@ -1,7 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Code, Crossword
+
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello World. You're at the code-and-crossword index.")
+	code_questions = Code.objects.all()
+	code_count = 0
+	if code_questions:
+		for question in code_questions:
+			code_count += 1
+	crossword_questions = Crossword.objects.all()
+	crossword_count = 0
+	if crossword_questions:
+		for question in crossword_questions:
+			crossword_count += 1
+	context = {'code_questions': code_questions, 'crossword_questions': crossword_questions, 'code_count': code_count, 'crossword_count': crossword_count}
+	return render(request, 'code_and_crossword/index.html', context)
+
+def code(request, level):
+	try:
+		question = Code.objects.get(level=level)
+	except Code.DoesNotExist:
+		raise Http404("Coding challenge does not exist")
+
+	return render(request, 'code_and_crossword/code.html', {'question': question})
+	
+def crossword(request, level):
+	try:
+		question = Crossword.objects.get(level=level)
+	except Code.DoesNotExist:
+		raise Http404("Crossword puzzle does not exist")
+
+	return render(request, 'code_and_crossword/crossword.html', {'question': question})
+
+def leaderboard(request):
+	return HttpResponse("These people attempted the challenges: ")
